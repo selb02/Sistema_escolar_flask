bf7334d74f2a3885f29ff8a04443380738ef002d
joao <lelis.jm5@gmail.com>
2025-03-13 21:41:21 -0300

    CRUD completo

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, jsonify, request
 
 meuApp = Flask(__name__)
 
-alunos = {}
-professores = {}
-turmas = {}
+alunos = []
+professores = []
+turmas = []
 
-@meuApp.route('/alunos', method=['POST'])
+@meuApp.route('/aluno', methods=['POST'])
 def create_alunos():
     data = request.json
     aluno = {
@@ -22,7 +22,7 @@ def create_alunos():
     alunos.append(aluno)
     return jsonify(aluno), 201
 
-@meuApp.route('/professor', method=['POST'])
+@meuApp.route('/professor', methods=['POST'])
 def create_professor():
     data = request.json
     professor = {
@@ -35,7 +35,7 @@ def create_professor():
     professores.append(professor)
     return jsonify(professor), 201
 
-@meuApp.route('/turma', method=['POST'])
+@meuApp.route('/turma', methods=['POST'])
 def create_turma():
     data = request.json
     turma = {
@@ -46,8 +46,124 @@ def create_turma():
     }
     turmas.append(turma)
     return jsonify(turma), 201
-    
 
+
+
+
+@meuApp.route('/aluno', methods=['GET'])
+def get_alunos():
+    return jsonify({'alunos': alunos})
+
+@meuApp.route('/professor', methods=['GET'])
+def get_professores():
+    return jsonify({'professores': professores})
+
+@meuApp.route('/turma', methods=['GET'])
+def get_turmas():
+    return jsonify({'turmas': turmas})
+
+
+
+
+
+@meuApp.route('/aluno/<int:aluno_id>', methods=['GET'])
+def get_aluno(aluno_id):
+    for aluno in alunos:
+        if aluno['id'] == aluno_id:
+            return jsonify(aluno)
+    return jsonify({'mensagem': 'Aluno não encontrado'}), 404
+
+@meuApp.route('/professor/<int:professor_id>', methods=['GET'])
+def get_professor(professor_id):
+    for professor in professores:
+        if professor['id'] == professor_id:
+            return jsonify(professor)
+    return jsonify({'mensagem': 'Professor não encontrado'}), 404
+
+@meuApp.route('/turma/<int:turma_id>', methods=['GET'])
+def get_turma(turma_id):
+    for turma in turmas:
+        if turma['id'] == turma_id:
+            return jsonify(turma)
+    return jsonify({'mensagem': 'Turma não encontrada'}), 404
+
+
+
+@meuApp.route('/aluno/<int:user_id>', methods=['DELETE'])
+def delete_aluno(aluno_id):
+    for aluno in alunos:
+        if aluno['id'] == aluno_id:
+            alunos.remove(aluno)
+            return jsonify({'mensagem': 'Aluno removido'})
+    return jsonify({'mensagem': 'Aluno não encontrado'}), 404
+
+@meuApp.route('/professor/<int:user_id>', methods=['DELETE'])
+def delete_professor(professor_id):
+    for professor in professores:
+        if professor['id'] == professor_id:
+            professores.remove(professor)
+            return jsonify({'mensagem': 'Professor removido'})
+    return jsonify({'mensagem': 'Professor não encontrado'}), 404
+
+@meuApp.route('/turma/<int:user_id>', methods=['DELETE'])
+def delete_turma(turma_id):
+    for turma in turmas:
+        if turma['id'] == turma_id:
+            turmas.remove(turma)
+            return jsonify({'mensagem': 'Turma removida'})
+    return jsonify({'mensagem': 'Turma não encontrada'}), 404
+
+
+
+
+@meuApp.route('/aluno/<int:aluno_id>', methods=['PUT'])
+def update_aluno(aluno_id):
+    for aluno in alunos:
+        if aluno['id'] == aluno_id:
+            dados = request.json
+            campos = [
+                'nome', 'idade', 'turma_id', 'data_nascimento',
+                'nota_primeiro_semestre', 'nota_segundo_semestre', 'media_final'
+            ]
+            for campo in campos:
+                novo_valor = dados.get(campo)
+                if novo_valor: 
+                    aluno[campo] = novo_valor
+            return jsonify(aluno)
+    
+    return jsonify({'mensagem': 'Aluno não encontrado'}), 404
+
+@meuApp.route('/professor/<int:aluno_id>', methods=['PUT'])
+def update_professor(professor_id):
+    for professor in professores:
+        if professor['id'] == professor_id:
+            dados = request.json
+            campos = [
+                    'nome', 'idade', 'materia', 'observacoes'
+            ]
+            for campo in campos:
+                novo_valor = dados.get(campo)
+                if novo_valor: 
+                    professor[campo] = novo_valor
+            return jsonify(professor)
+    
+    return jsonify({'mensagem': 'professor não encontrado'}), 404
+
+@meuApp.route('/turma/<int:aluno_id>', methods=['PUT'])
+def update_turma(turma_id):
+    for turma in turmas:
+        if turma['id'] == turma_id:
+            dados = request.json
+            campos = [
+                    'descricao', 'professor_id', 'ativo'
+            ]
+            for campo in campos:
+                novo_valor = dados.get(campo)
+                if novo_valor: 
+                    turma[campo] = novo_valor
+            return jsonify(turma)
+    
+    return jsonify({'mensagem': 'turma não encontrada'}), 404
 
 
 
